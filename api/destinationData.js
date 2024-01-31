06eaf55cba4e1a3aa6843b752a17f4f0a84e20fe
Bruce Clark <Bruce.clark55@gmail.com>
2024-01-30 21:05:18 -0500

    added destination promises

---
+++ api/destinationData.js
@@ -0,0 +1,79 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getDestinations = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/destination.json?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const deleteDestination = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/destination/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve((data)))
+    .catch(reject);
+});
+
+const getSingleDestination = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/destination/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const createDestination = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/destination.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateDestination = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/destination/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getDestinations,
+  createDestination,
+  updateDestination,
+  getSingleDestination,
+  deleteDestination,
+};
