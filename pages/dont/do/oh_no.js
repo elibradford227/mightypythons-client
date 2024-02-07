2d108467e07e39e44315ae3e9ae1f9cfdaad30c8
Michael Brantley <brantleyrecs@gmail.com>
2024-02-07 17:38:38 -0600

    fire works

--- pages/dont/do/oh_no.js
+++ pages/dont/do/oh_no.js
@@ -4,17 +4,21 @@ import Sheep from '../../../components/dontOpen/Sheep';
 export default function OhNo() {
   return (
     <div>
-      <div className="cardMover3">
-        <div className="warnedCard3">
-          <header style={{
-            color: 'black', fontSize: '25px', textAlign: 'center', marginTop: '10px',
-          }}
-          >
-            GET HIM BEFORE HE GETS YOU!!!
-          </header>
+      <div>
+        <div>
+          <div className="cardMover3">
+            <div className="warnedCard3">
+              <header style={{
+                color: 'black', fontSize: '25px', textAlign: 'center', marginTop: '10px',
+              }}
+              >
+                GET HIM BEFORE HE GETS YOU!!!
+              </header>
+            </div>
+          </div>
+          <Sheep />
         </div>
       </div>
-      <Sheep />
     </div>
   );
 }
